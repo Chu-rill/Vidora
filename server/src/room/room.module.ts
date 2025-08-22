ab1d5738a640c71f3fcadce5d8a041b981150377
Chu-rill <churchillrimamskep@gmail.com>
2025-08-22 17:49:44 -0400

    doc:finished auth'email' and user doc in swagger

--- server/src/room/room.module.ts
+++ server/src/room/room.module.ts
@@ -3,11 +3,12 @@ import { RoomService } from './room.service';
 import { RoomController } from './room.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { RoomRepository } from './room.repository';
+import { RoomAdminController } from './room.admin.controller';
 
 @Module({
   imports: [PrismaModule],
   providers: [RoomService, RoomRepository],
-  controllers: [RoomController],
+  controllers: [RoomController, RoomAdminController],
   exports: [RoomService],
 })
 export class RoomModule {}
